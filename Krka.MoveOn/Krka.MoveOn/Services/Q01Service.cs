28a348bcd84b98120bc75fa2c8ac687dd87e0c97
Samuel Vestur <samuel.vestur@karatnet.com>
2024-10-08 15:17:00 +0200

    -?

---
+++ Krka.MoveOn/Krka.MoveOn/Services/Q01Service.cs
@@ -0,0 +1,18 @@
+﻿using Krka.MoveOn.Data;
+using Krka.MoveOn.Data.Questionnaires;
+using Microsoft.EntityFrameworkCore;
+
+namespace Krka.MoveOn.Services
+{
+    public class Q01Service(ApplicationDbContext context)
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<List<QuestionnaireGeneral01>> GetQuestionnairesAsync()
+        {
+            return await _context.QuestionnaireGeneral01s.ToListAsync();
+        }
+
+
+    }    
+}
