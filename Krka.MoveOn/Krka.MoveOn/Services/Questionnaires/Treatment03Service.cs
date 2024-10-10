0d8210a009db41dd4246ba2906bbfdd6e8718cdc
budnykihor <ihor.budnyk@karatnet.com>
2024-10-10 13:53:25 +0200

    Pridane services (copy for yevhenii)

---
+++ Krka.MoveOn/Krka.MoveOn/Services/Questionnaires/Treatment03Service.cs
@@ -0,0 +1,15 @@
+﻿using Krka.MoveOn.Data;
+using Krka.MoveOn.Data.Dials;
+using Microsoft.EntityFrameworkCore;
+
+namespace Krka.MoveOn.Services.Questionnaires;
+
+public class Treatment03Service(ApplicationDbContext context)
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<List<DialActiveIngredient>> GetActiveIngredient() { 
+        var list = await _context.DialActiveIngredients.Where(i => i.Type_q == 2).ToListAsync();
+        return list;
+    }
+}
