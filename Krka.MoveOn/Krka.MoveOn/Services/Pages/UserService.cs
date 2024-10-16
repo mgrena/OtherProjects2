ccb10a8d168e2ff7d6c859a26f7cc22ee1b5be8e
Martin Greňa <martin.grena@karatnet.com>
2024-10-16 10:17:42 +0200

    - experimenty so štýlmi a editovaním

---
+++ Krka.MoveOn/Krka.MoveOn/Services/Pages/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Krka.MoveOn.Services.Pages;
+
+public class UserService
+{
+}
+
+public class RoleObject
+{
+    public required string Name { get; set; }
+}
