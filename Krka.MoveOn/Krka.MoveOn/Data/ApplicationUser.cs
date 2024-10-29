b3e306a578d1d1933996aba3c2282884ce344fb1
Martin Greňa <martin.grena@karatnet.com>
2024-10-29 08:13:46 +0100

    - ďalšie práce na administrácii používateľov

--- Krka.MoveOn/Krka.MoveOn/Data/ApplicationUser.cs
+++ Krka.MoveOn/Krka.MoveOn/Data/ApplicationUser.cs
@@ -1,32 +1,25 @@
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using Krka.MoveOn.Services.Pages;
 
-namespace Krka.MoveOn.Data {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser {
-        [Column("first_name", TypeName = "nvarchar"), StringLength(100)]
-        public string? FirstName { get; set; }
-        [Column("lst_name", TypeName = "nvarchar"), StringLength(100)]
-        public string? LastName { get; set; }
-        [Column("title_before", TypeName = "nvarchar"), StringLength(50)]
-        public string? TitleBefore { get; set; }
-        [Column("title_after", TypeName = "nvarchar"), StringLength(50)]
-        public string? TitleAfter { get; set; }
-    }
-    public class ApplicationEditUser
-    {
-        public required string Id { get; set; }
-        public required string UserName { get; set; }
-        public string? Password { get; set; }
-        public string? ConfirmPassword { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? TitleBefore { get; set; }
-        public string? TitleAfter { get; set; }
-        public string? Phone { get; set; }
-        public RoleObject Role { get; set; } = new RoleObject() { Name = "Doctor" };
-        public string? FullName { get { return string.Join(" ", new[] { TitleBefore, FirstName, LastName, TitleAfter }.Where(i => !string.IsNullOrWhiteSpace(i))); } }
-    }
+namespace Krka.MoveOn.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser {
+    [Column("first_name", TypeName = "nvarchar"), StringLength(100)]
+    public string? FirstName { get; set; }
+    [Column("lst_name", TypeName = "nvarchar"), StringLength(100)]
+    public string? LastName { get; set; }
+    [Column("title_before", TypeName = "nvarchar"), StringLength(50)]
+    public string? TitleBefore { get; set; }
+    [Column("title_after", TypeName = "nvarchar"), StringLength(50)]
+    public string? TitleAfter { get; set; }
+    [NotMapped]
+    public string? Password { get; set; }
+    [NotMapped]
+    public string? ConfirmPassword { get; set; }
+    [NotMapped]
+    public string? FullName { get { return string.Join(" ", new[] { TitleBefore, FirstName, LastName, TitleAfter }.Where(i => !string.IsNullOrWhiteSpace(i))); } }
+    [NotMapped]
+    public IdentityRole? Role { get; set; }
 }
