06c8ea05b900dceddcba5cc8d05f2fc34992aec3
budnykihor <ihor.budnyk@karatnet.com>
2025-03-28 12:24:14 +0100

    --InformedConsentCompetence created

--- Servier.Pressure/Servier.Pressure/Services/Pages/QuestionnaireService.cs
+++ Servier.Pressure/Servier.Pressure/Services/Pages/QuestionnaireService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Servier.Pressure.Data;
 using Servier.Pressure.Data.Lists;
 using Servier.Pressure.Data.Models;
@@ -11,6 +12,59 @@ public class QuestionnaireService(ApplicationDbContext context, ILogger<Question
     private readonly ApplicationDbContext _context = context;
     private readonly ILogger _logger = logger;
 
+    public async Task<InformedConsentCompetence> GetWorkplaceByUserIdAsync(string Id)
+    {
+        InformedConsentCompetence? InformedConsentCompetenceQuestionary = await _context.InformedConsentCompetences.FirstOrDefaultAsync(p => p.Id == Id);
+        InformedConsentCompetenceQuestionary ??= new() {
+            Id = string.Empty,
+            IsConsent = true,
+            IsAdult = true,
+            IsStableHypertension = true,
+            IsInformedConsent = true,
+            IsAlreadyEnrolled = true,
+            IsResistantHypertension = true,
+            IsClinicalTrial = true,
+            IsPregnant = true,
+            VisitDate = DateTime.Now,
+            ModifiedAt = DateTime.Now,
+            CreatedAt = DateTime.Now,
+        };
+        return InformedConsentCompetenceQuestionary;
+    }
+
+    public async Task<OperationResult> SaveInformedConsentCompetenceQuestionaryAsync(InformedConsentCompetence informedconsentcompetence)
+    {
+        var InformedConsentCompetence = await _context.InformedConsentCompetences.FirstOrDefaultAsync(i => i.Id == informedconsentcompetence.Id);
+        if (InformedConsentCompetence == null)
+        {
+            _logger.LogInformation("The informed consent competence for patient {user} has been added.", informedconsentcompetence.Id);
+            _context.InformedConsentCompetences.Add(informedconsentcompetence);
+        }
+        else
+        {
+            _logger.LogInformation("The informed consent competence with id {id} has been updated.", informedconsentcompetence.Id);
+            InformedConsentCompetence.IsConsent = informedconsentcompetence.IsConsent;
+            InformedConsentCompetence.IsAdult = informedconsentcompetence.IsAdult;
+            InformedConsentCompetence.IsStableHypertension = informedconsentcompetence.IsStableHypertension;
+            InformedConsentCompetence.IsInformedConsent = informedconsentcompetence.IsInformedConsent;
+            InformedConsentCompetence.IsAlreadyEnrolled = informedconsentcompetence.IsAlreadyEnrolled;
+            InformedConsentCompetence.IsResistantHypertension = informedconsentcompetence.IsResistantHypertension;
+            InformedConsentCompetence.IsClinicalTrial = informedconsentcompetence.IsClinicalTrial;
+            InformedConsentCompetence.IsPregnant = informedconsentcompetence.IsPregnant;
+
+        }
+
+        try
+        {
+            await _context.SaveChangesAsync();
+            return OperationResult.SuccessResult();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            return OperationResult.FailureResult("Pri ukladaní údajov došlo k neočakávanej chybe.", ex.Message);
+        }
+    }
     public async Task<TreatmentBefore?> GetTreatmentBeforeAsync(string patientId)
     {
         return await _context.TreatmentsBefore.FirstOrDefaultAsync(i => i.Id == patientId);
